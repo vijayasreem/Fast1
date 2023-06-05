f828914d865d407eb2b60f7950e60fc57cbc5a79
vijayasreem <133225735+vijayasreem@users.noreply.github.com>
2023-06-05 11:12:52 +0530

    initial commit

---
+++ app/main.py
@@ -0,0 +1,34 @@
+Fast API:
+
+1. Navigate to sacral.ai website and login:
+   - This API will allow users to navigate to the sacral.ai website and login successfully.
+
+2. Access “Expert Services to Change Business” page:
+   - This API will allow users to access the “Expert Services to Change Business” page after login.
+
+3. Configure GitHub:
+   - This API will allow users to click on Configure and then on GitHub.
+   - The user must provide a GitHub username, password, URL and repository name.
+   - The user must be able to click on the reset button to clear the details entered in the Pop Up form.
+   - The user must be able to click on the save button to configure.
+   - The Java API must validate the Jira Software credentials and return a response indicating whether authentication was successful or not.
+   - If the credentials are not valid, an error message must be displayed, and the user must be prompted to enter the correct information.
+
+4. Display list with Title, User Name, URL, Action:
+   - This API will allow users to display the list with Title, User Name, URL, and Action.
+   - The user must be able to click on the edit button and make changes to the Title, User Name, and URL.
+   - The user must be able to click on the delete button to delete a particular title from the list.
+
+5. Change No of entries to display in list:
+   - This API will allow users to change the number of entries to display in the list while clicking on the dropdown button in “Show entries”.
+
+6. Display pagination under list:
+   - This API will allow users to display pagination under the list.
+   - The user must be able to click on pagination to change the page numbers.
+
+7. Add more button to configure other GitHub:
+   - This API will allow users to click on the Add more button above the list to configure other GitHub.
+   - A Pop Up form must be displayed when the user clicks on the Add more button.
+   - The user must be able to enter the required details in the Pop Up form.
+   - The user must be able to click on the reset button to clear the details entered in the Pop Up form.
+   - The user must be able to click on the save button to configure.
